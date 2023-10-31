7f385f84cdfade185206b028250458b8ecd91688
lhb <ldotoroo@naver.com>
2023-11-01 02:18:14 +0900

    모바일 터치 이벤트 추가

--- js/index.js
+++ js/index.js
@@ -2,12 +2,11 @@
 let pageMoveBtn = Array.from(document.querySelectorAll('.pageMoveBtn'))
 let pageSection = Array.from(document.querySelectorAll('.page'))
 let index = 0
-
 function btnClickPageMove () {
 	for(let i = 0; i < pageMoveBtn.length; i++) {
 		pageMoveBtn[i].addEventListener('click' , function(e) {
 			e.preventDefault()
-			index = e.target.getAttribute('data-index')
+			index = e.target.getAttribute('data-btnIdx')
 
 			let filteredPage = pageSection.filter((item,idx) => idx < index)
 			let totalHeight = filteredPage.reduce((accumulator,currentHeight) => {
@@ -29,9 +28,11 @@ btnClickPageMove()
 
 
 /* 스크롤시 섹션이동 기능 */
+let scrolling = true;
 
+/* pc용 스크롤 이벤트 */
 function scrollPageMove(e) {
-	
+	if(scrolling) {
 		if(e.deltaY > 0){
 			if(index < pageSection.length - 1) {
 				index ++
@@ -52,9 +53,56 @@ function scrollPageMove(e) {
 			pageSection[i].classList.remove('active')
 		}
 		pageSection[index].classList.add('active')
-	
+
+		scrolling = false;
+		setTimeout(() => {
+			scrolling = true
+		}, 600)
+	}
 }
 
+/* 모바일용 터치 이벤트 */
+let startScreenY;
+let endScreenY;
+
+function touchEvent () {
+	document.body.addEventListener('touchstart', (e) => {
+	startScreenY = e.changedTouches[0].screenY;
+	});
+	document.body.addEventListener('touchend', (e) => {
+		endScreenY = e.changedTouches[0].screenY;
+		if(scrolling) {
+			if(startScreenY > endScreenY) {
+				if(index < pageSection.length - 1) {
+					index ++
+				}
+			} else {
+				if(index > 0)
+				index --
+			}
+			console.log(index)
+			let filteredPage = pageSection.filter((item , idx) => idx < index)
+			let totalHieght = filteredPage.reduce((accumulator,currentHeight) => {
+				return accumulator + currentHeight.clientHeight
+			}, 0)
+
+			window.scrollTo({top: totalHieght , behavior : "smooth"}); 
+
+			for(let i = 0; i < pageSection.length; i ++) {
+				pageSection[i].classList.remove('active')
+			}
+			pageSection[index].classList.add('active')
+
+			scrolling = false;
+			setTimeout(() => {
+				scrolling = true
+			}, 600)
+		}
+		btnClassAdd()
+	});
+	
+}
+touchEvent()
 
 /* 버튼에 active 클래스 추가 함수 */
 function btnClassAdd() {
@@ -66,7 +114,6 @@ function btnClassAdd() {
 		}
 	} 
 }
-
 
 /* 새로고침시 버튼에 active클래스 추가 */
 function btnReloadClassAdd() {
@@ -101,7 +148,6 @@ function mobileGnbClose() {
 	})
 }
 mobileGnbClose()
-
 
 /* 함수실행 */
 window.addEventListener('wheel', function(e) {
